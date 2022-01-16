d3054f57693daec09a19653c791799166f6f7ebd
Wyatt Marvil <2020wmarvil@gmail.com>
2022-01-15 22:12:06 -0500

    Mesh generation

--- Planets/Assets/Scripts/Planet.cs
+++ Planets/Assets/Scripts/Planet.cs
@@ -1,18 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Planet : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Planet : MonoBehaviour {
+	[Range(2, 256)]
+	[SerializeField] int resolution = 10;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField, HideInInspector] MeshFilter[] meshFilters;
+	TerrainFace[] terrainFaces;
+
+	void OnValidate() {
+		Initialize();
+		GenerateMesh();
+	}
+
+	void Initialize() {
+		if (meshFilters == null || meshFilters.Length == 0) meshFilters = new MeshFilter[6];
+		terrainFaces = new TerrainFace[6];
+
+		Vector3[] directions = {
+			Vector3.up,
+			Vector3.down,
+			Vector3.left,
+			Vector3.right,
+			Vector3.forward,
+			Vector3.back
+		};
+
+		for (int i=0; i<6; i++) {
+			if (meshFilters[i] == null) {
+				GameObject meshObj = new GameObject("mesh");
+				meshObj.transform.parent = transform;
+
+				meshObj.AddComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+				meshFilters[i] = meshObj.AddComponent<MeshFilter>();
+				meshFilters[i].sharedMesh = new Mesh();
+			}
+
+			terrainFaces[i] = new TerrainFace(meshFilters[i].sharedMesh, resolution, directions[i]);
+		}
+	}
+
+	void GenerateMesh() {
+		for (int i=0; i<terrainFaces.Length; i++) {
+			terrainFaces[i].ConstructMesh();
+		}
+	}
 }
