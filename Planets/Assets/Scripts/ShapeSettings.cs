09eab739d2c4b1135180c2a833c6ed8a0819a451
Wyatt Marvil <2020wmarvil@gmail.com>
2022-01-15 23:27:50 -0500

    NoiseFilter for terrain generation

--- Planets/Assets/Scripts/ShapeSettings.cs
+++ Planets/Assets/Scripts/ShapeSettings.cs
@@ -3,4 +3,12 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ShapeSettings : ScriptableObject {
 	public float planetRadius = 1f;
+	public NoiseLayer[] noiseLayers;
+
+	[System.Serializable]
+	public class NoiseLayer {
+		public bool enabled = true;
+		public bool useFirstLayerAsMask;
+		public NoiseSettings noiseSettings;
+	}
 }
